f78d71cba34199245308f7083a36706cf7250e39
roeniss.milk <roeniss.milk@kakaocorp.com>
2021-05-22 17:24:00 +0900

    update

--- config/SafeCrossWalk/views.py
+++ config/SafeCrossWalk/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def index(req):
+  # to FE : "give a unique id for each user"
+  
+  # get : UniqueID / Lan / Long
+
+  # save (overwrite) user to server
+
+  # Send notification to nearby cars
+  cars = collect_cars()
+
+  return JsonResponse('foo':'bar', "hello": "world", "nested" : {"a":1, "b":2 } } ) 
+
+def collect_cars():
+  pass
