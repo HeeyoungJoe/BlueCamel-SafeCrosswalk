89f2df29d44f820e34aeff7cf92440956aaf42ef
leeju <slime0519@gist.ac.kr>
2021-05-23 01:30:51 +0900

    develop simple braking model for car

---
+++ ProcessData/BrakingModules/simplecheckbraking.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+LATENCY = 1
+DRIVER_RESPONSE = 1
+
+
+def simplemodel(initspeed, brake_acc = 3 ):
+
+    vehicle_Initial_Speed = initspeed / 3.6;
+    vehicle_Mass = 1145;
+
+    distance = (vehicle_Initial_Speed**2)/(2*brake_acc)
+    time = vehicle_Initial_Speed/brake_acc
+    return distance, time
+
+def simplecheckBraking(velocity, distance, safe_time = 5):
+    braking_distance, arrival_time = simplemodel(velocity)
+
+    safe_distance = (safe_time + LATENCY+DRIVER_RESPONSE) * velocity + braking_distance
+    critical_distance = (LATENCY+DRIVER_RESPONSE)*velocity + braking_distance
+
+    if distance < critical_distance:
+        return 1
+    if distance <= safe_distance :
+        maxtime = safe_time            # interval : [t_L + t_d <= t <= (dis_safe-dis_brake)/velocity)
+        mintime = (LATENCY + DRIVER_RESPONSE)
+        x = (distance-braking_distance)/velocity - (maxtime + mintime)/2
+        return 1-(1/(1+np.exp(-x)))
+    return 0
+
+
+
+print(simplemodel(100))
+
