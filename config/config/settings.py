af2431be8b694799a7b3c8a76b4cb6826e9c2bb9
roeniss.milk <roeniss.milk@kakaocorp.com>
2021-05-23 03:38:05 +0900

    update

--- config/config/settings.py
+++ config/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rxm56^zlyc(^2hlu&xu&)@qa0jn^$)hu2$p^bz07(4b+^$bc14
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
